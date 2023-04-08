b9f9622911f291db394b28019c298f7d003f3c8c
Maksxpl <maks.kozon@gmail.com>
2023-04-08 21:23:24 +0200

    Would be cool to update bot with discord command [2/2] + branch-set

--- Abominable-Intelligence.py
+++ Abominable-Intelligence.py
@@ -1,6 +1,8 @@
 import logging
 from random import randint
 
+import sys
+import traceback
 import os
 import subprocess
 
@@ -36,24 +38,46 @@ bot = interactions.Client(
 async def _dice(ctx: interactions.CommandContext):
     await ctx.send(randint(1,99))
 
+#todo? merge both branch functions into one
+@bot.command(
+    name="git-branch-check",
+    description="check remote branches",
+)
+async def _branch_check(ctx: interactions.CommandContext):
+        a = subprocess.check_output(['git', 'branch', '-r'])
+        await ctx.send(a.decode())
+        
+@bot.command(
+    name="git-branch-set",
+    description="sets active branch",
+)
+async def _branch_check(ctx: interactions.CommandContext, branch: str):
+        a = subprocess.check_output(['git', 'checkout', branch])
+        await ctx.send(a.decode())
 
 @bot.command(
     name="update",
     description="Update the bot",
 )
-async def _dice(ctx: interactions.CommandContext):
-    ctx.send("Pulling code from github...")
-    try:
-        a = subprocess.check_output(['git', 'pull'])
-        await ctx.send(a)
-    except Exception as Err:
-        await ctx.send("Pull failed")
-        await ctx.send(f"Traceback:\n{Err}")
-    await ctx.send("Restarting the bot...")
-    try:
-        await ctx.send("[Placeholder] Restart not implemented yet")
-    except:
-        await ctx.send("Restart failed")
+async def _update(ctx: interactions.CommandContext):
+        await ctx.send("Pulling code from github...")
+        try:
+                a = subprocess.check_output(['git', 'pull'])
+                await ctx.send(a.decode("ascii"))
+
+                # if updated successfully, restart the bot
+                await ctx.send("Restarting the bot...")
+                try:
+                        os.execv(sys.executable, ['python'] + sys.argv)
+                        await ctx.send("Restart succeeded(?)") # todo: move it to argument
+                except Exception:
+                        await ctx.send(f"Traceback:\n{traceback.format_exc()}")
+                        await ctx.send("Restart failed")
+
+        except Exception:
+            await ctx.send("Pull failed")
+            await ctx.send(f"Traceback:\n{traceback.format_exc()}")
+
 
 @bot.event
 async def on_ready():
