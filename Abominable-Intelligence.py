a286ee39811a71bf5ff7939540a1fc0e662aa306
root <root@nukeops.com>
2023-04-08 16:52:19 +0200

    Would be cool to update bot with discord command [1/2]

--- Abominable-Intelligence.py
+++ Abominable-Intelligence.py
@@ -1,8 +1,13 @@
 import logging
 from random import randint
 
+import os
+import subprocess
+
 import interactions
 import yaml
+
+os.chdir("/opt/Abominable-Intelligence")
 
 ## logs for journal
 DEBUG, INFO, WARN, ERROR, SUCCESS = range(1, 6)
@@ -11,11 +16,11 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-# config
+
 with open("config.yaml") as conf:
-	f = yaml.load(conf, Loader=yaml.FullLoader)
-	bot_token = f["bot_token"]
-	guild_id = f["guild_id"]
+        f = yaml.load(conf, Loader=yaml.FullLoader)
+        bot_token = f["bot_token"]
+        guild_id = f["guild_id"]
 
 
 bot = interactions.Client(
@@ -23,20 +28,37 @@ bot = interactions.Client(
     default_scope=guild_id,
 )
 
-##
-## functions
-##
 
-# dice
-@bot.command(name="dice",
-             description="1d100")
+@bot.command(
+    name="dice",
+    description="funny dice",
+)
 async def _dice(ctx: interactions.CommandContext):
-    await ctx.send(randint(1,100))
+    await ctx.send(randint(1,99))
 
-   
+
+@bot.command(
+    name="update",
+    description="Update the bot",
+)
+async def _dice(ctx: interactions.CommandContext):
+    ctx.send("Pulling code from github...")
+    try:
+        a = subprocess.check_output(['git', 'pull'])
+        await ctx.send(a)
+    except Exception as Err:
+        await ctx.send("Pull failed")
+        await ctx.send(f"Traceback:\n{Err}")
+    await ctx.send("Restarting the bot...")
+    try:
+        await ctx.send("[Placeholder] Restart not implemented yet")
+    except:
+        await ctx.send("Restart failed")
+
 @bot.event
 async def on_ready():
     print("Bot started, I think")
     logger.log(INFO, 'Abominable intelligence has started!')
 
-    bot.start()
+
+bot.start()
