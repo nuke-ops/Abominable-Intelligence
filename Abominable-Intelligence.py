f4c6a909a36bb5d3937f4ccbcfafe92f59df0c3d
Maksxpl <maks.kozon@gmail.com>
2023-04-29 05:30:06 +0200

    Update Abominable-Intelligence.py
    updated decorator names to current Interactions standards, cleaned some stuff and added options for dice commands

--- Abominable-Intelligence.py
+++ Abominable-Intelligence.py
@@ -1,14 +1,14 @@
 import logging
-from random import randint
-
-import sys
-import traceback
 import os
 import subprocess
+import sys
+from random import randint
 
-import interactions
 import yaml
+from interactions import (Client, OptionType, SlashCommandChoice, SlashContext,
+                          slash_command, slash_option, subcommand)
 
+# set os path to bot location for git related commands
 os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
 
 ## logs for journal
@@ -18,52 +18,50 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-
+# load bot token and guild id
 with open("config.yaml") as conf:
         f = yaml.load(conf, Loader=yaml.FullLoader)
         bot_token = f["bot_token"]
         guild_id = f["guild_id"]
 
-
-bot = interactions.Client(
+# define bot and its settings
+bot = Client(
     token=bot_token,
     default_scope=guild_id,
 )
 
+##
+## functions
+##
+
 def list_branches():
-    branches = subprocess.check_output('git branch -r'.split()).decode
+    branches = subprocess.check_output('git branch -r'.split()).decode # example output: origin/HEAD -> origin/main \n origin/main \n origin/v1
+    # remove head, remove the "origin/"" thingies and transform to list. example: ['main', 'v1']
     return [x.replace("origin/", "").replace("->", "").strip() for x in branches().split("\n") if x and "origin/HEAD" not in x]
 
-@bot.command(
-    name="dice",
-    description="roll 1d100",
-)
-async def dice(ctx: interactions.CommandContext):
-    await ctx.send(randint(1,100))
+##
+## git commands
+##
 
-
-@bot.command(description="GitHub management commands")
-async def git(ctx: interactions.CommandContext):
+@slash_command(description="GitHub management commands")
+async def git(ctx: SlashContext):
     pass
 
-@git.subcommand(description="check remote branches")
-async def branches(ctx: interactions.CommandContext):
+@subcommand("git", description="check remote branches")
+async def branches(ctx: SlashContext):
     branches = subprocess.check_output(['git', 'branch', '-r']).decode()
     await ctx.send(branches)
 
-@git.subcommand(description="switch active branch")
-@interactions.option(required=True, choices = [interactions.Choice(name=branch, value=branch) for branch in list_branches()])
-async def checkout(ctx: interactions.CommandContext, branch: str):
+@subcommand("git", description="switch active branch")
+@slash_option(name="repo", description="eh?", opt_type=OptionType.STRING, required=True, choices = [SlashCommandChoice(name=branch, value=branch) for branch in list_branches()])
+async def checkout(ctx: SlashContext, branch: str):
     branch_set = subprocess.check_output(["git", "checkout", branch]).decode()
     branch_current = subprocess.check_output(["git", "branch", "--show-current"]).decode()
     await ctx.send(f"{branch_set}\nCurrent branch: {branch_current}")
 
 
-@bot.command(
-    name="update",
-    description="Update the bot",
-)
-async def _update(ctx: interactions.CommandContext):
+@slash_command(description="Update the bot")
+async def pull(ctx: SlashContext):
         await ctx.send("Pulling code from github...")
         try:
                 pull = subprocess.check_output(['git', 'pull']).decode("ascii")
@@ -73,14 +71,33 @@ async def _update(ctx: interactions.CommandContext):
                 await ctx.send("Restarting the bot...")
                 try:
                         os.execv(sys.executable, ['python'] + sys.argv)
-                        await ctx.send("Restart succeeded(?)") # todo: move it to argument
+                        await ctx.send("Restart succeeded(?)") # todo: pass it as sys.argv and ctx.send after the restart
                 except Exception:
-                        await ctx.send(f"Traceback:\n{traceback.format_exc()}")
                         await ctx.send("Restart failed")
 
         except Exception:
             await ctx.send("Pull failed")
-            await ctx.send(f"Traceback:\n{traceback.format_exc()}")
+
+##
+## fun commands or something idk
+##
+
+# rolls a dice
+@slash_command(description="funny dice")
+@slash_option(
+    name="dice",
+    description="Dice",
+    required=True,
+    opt_type=OptionType.INTEGER
+)
+@slash_option(
+    name="sides",
+    description="Sides",
+    required=True,
+    opt_type=OptionType.INTEGER
+)
+async def dice(ctx: SlashContext, dice:int, sides:int):
+    await ctx.send(f"{dice}d{sides}: {', '.join([str(randint(1,sides)) for _ in range(dice)])}")
 
 
 @bot.event
