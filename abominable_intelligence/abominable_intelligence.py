3c618140a2a84e4e8cb2ff645e21f84a1d29b3a6
Maksxpl <maks.kozon@gmail.com>
2023-05-03 20:55:21 +0200

    Update abominable_intelligence.py

--- abominable_intelligence/abominable_intelligence.py
+++ abominable_intelligence/abominable_intelligence.py
@@ -11,10 +11,14 @@ from global_variables import script_dir, config_path
 os.chdir(script_dir)
 
 # load bot token and guild id
-with open(config_path) as conf:
-    f = yaml.load(conf, Loader=yaml.FullLoader)
-    bot_token = f["bot_token"]
-    guild_id = f["guild_id"]
+try:
+    with open(config_path) as conf:
+        f = yaml.load(conf, Loader=yaml.FullLoader)
+        bot_token = f["bot_token"]
+        guild_id = f["guild_id"]
+except FileNotFoundError:
+    bot_token=os.environ.get('DISCORD_BOT_TOKEN')
+    guild_id=os.environ.get('DISCORD_GUILD_ID)')
 
 
 # logs for journal
@@ -27,8 +31,8 @@ logger = logging.getLogger(__name__)
 
 # define bot and its settings
 bot = Client(
-    token=os.environ.get('DISCORD_BOT_TOKEN', bot_token),
-    default_scope=os.environ.get('DISCORD_GUILD_ID)', guild_id),
+    token=(bot_token),
+    default_scope=(guild_id),
     # delete_unused_application_cmds=True
 )
 
