6ad6c48428826876037174f79d4bb1da76650082
Maksxpl <maks.kozon@gmail.com>
2023-05-03 19:49:15 +0100

    Delete Abominable_intelligence directory

--- Abominable_intelligence/global_variables.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-script_dir = os.path.dirname(os.path.abspath(__file__))
-config_path = os.path.join(script_dir, "config.yaml")
-
-# todo I guess: move to config and make function accessible only for owner to change these
-role_id_administration = 668245500612444205
-logs_channel = 732923500624347181
