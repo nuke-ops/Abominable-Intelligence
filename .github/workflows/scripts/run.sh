a10d7e7f332e06852b3a7c2894fcac5d598a6742
Maksxpl <maks.kozon@gmail.com>
2023-08-11 17:09:26 +0200

    Update run.sh

--- .github/workflows/scripts/run.sh
+++ .github/workflows/scripts/run.sh
@@ -3,11 +3,14 @@
 # Start the bot in the background
 python abominable_intelligence/abominable_intelligence.py &
 
+# Capture the PID of the started process
+BOT_PID=$!
+
 # Wait for a few seconds for the bot to start up
 sleep 5
 
 # Check if the bot is still running
-if pgrep -f "python abominable_intelligence/abominable_intelligence.py" >/dev/null
+if ps -p $BOT_PID > /dev/null
 then
     echo "status=success" >> $GITHUB_ENV
 else
@@ -19,11 +22,14 @@ fi
 # Wait for a few seconds for the bot to run
 sleep 5
 
-# Stop the bot
-pkill -f "python abominable_intelligence/abominable_intelligence.py"
+# Stop the bot using the captured PID
+kill $BOT_PID
+
+# Wait for the process to fully terminate
+wait $BOT_PID
 
 # Check if the bot has stopped
-if pgrep -f "python abominable_intelligence/abominable_intelligence.py" >/dev/null
+if ps -p $BOT_PID > /dev/null
 then
     echo "status=failure" >> $GITHUB_ENV
     echo "Error: Bot failed to stop"
