f65a168e032b144c898046eeeceb83292a1b89b3
Maksxpl <maks.kozon@gmail.com>
2023-08-24 23:37:46 +0200

    uh, maybe

--- abominable_intelligence/extensions/MySQL.py
+++ abominable_intelligence/extensions/MySQL.py
@@ -2,44 +2,39 @@ import mysql.connector
 from cryptography.fernet import Fernet
 from data_manager import config_sql
 
-config_sql = config_sql()
-
 
 class Sql:
-    class gw2:
-        def __init__(self) -> None:
-            self.encrypt_key = Fernet(config_sql["encrypt_key"])
-            config_sql.pop(
-                "encrypt_key"
-            )  # remove the key so **config_sql can load entire thing without doing dumb work arounds 👍
+    config_sql = config_sql()
+    encrypt_key = Fernet(config_sql["encrypt_key"])
+    config_sql.pop("encrypt_key")
 
-        def insert(self, username: str, api_key: str):
-            with mysql.connector.connect(**config_sql) as connection:
+    class gw2:
+        @classmethod
+        def insert(cls, username: str, api_key: str):
+            with mysql.connector.connect(**Sql.config_sql) as connection:
                 with connection.cursor() as cursor:
                     query = "INSERT INTO gw2 (username, api_key) VALUES (%s, %s)"
-                    encrypted_api_key = self.encrypt_key.encrypt(
-                        bytes(api_key, "utf-8")
-                    )
+                    encrypted_api_key = Sql.encrypt_key.encrypt(bytes(api_key, "utf-8"))
                     cursor.execute(query, (username, encrypted_api_key))
                     connection.commit()
 
-        def update(self, username: str, api_key: str):
-            with mysql.connector.connect(**config_sql) as connection:
+        @classmethod
+        def update(cls, username: str, api_key: str):
+            with mysql.connector.connect(**Sql.config_sql) as connection:
                 with connection.cursor() as cursor:
                     query = "UPDATE gw2 SET api_key = %s WHERE username = %s"
-                    encrypted_api_key = self.encrypt_key.encrypt(
-                        bytes(api_key, "utf-8")
-                    )
+                    encrypted_api_key = Sql.encrypt_key.encrypt(bytes(api_key, "utf-8"))
                     cursor.execute(query, (encrypted_api_key, username))
                     connection.commit()
 
-        def select(self, username: str):
-            with mysql.connector.connect(**config_sql) as connection:
+        @classmethod
+        def select(cls, username: str):
+            with mysql.connector.connect(**Sql.config_sql) as connection:
                 with connection.cursor() as cursor:
                     query = "SELECT * FROM gw2 WHERE username = %s"
                     cursor.execute(query, [username])
                     rows = cursor.fetchall()
                 if rows:
-                    return self.encrypt_key.decrypt(rows[0][-1])
+                    return Sql.encrypt_key.decrypt(rows[0][-1])
                 else:
                     return None
