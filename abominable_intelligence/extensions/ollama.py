d3026c3284d3009514625db2fcb810a7a27db459
Maksxpl <maks.kozon@gmail.com>
2023-11-13 01:18:58 +0100

    modal settings

--- abominable_intelligence/extensions/ollama.py
+++ abominable_intelligence/extensions/ollama.py
@@ -3,11 +3,95 @@ import logging
 
 import data_manager
 import lightbulb
+import miru
 import requests
 from decorators import administration_only
 from extensions.core import error
 
 plugin = lightbulb.Plugin("ollama")
+
+
+class SettingsModal(miru.Modal):
+    settings = data_manager.data()["ai"]
+    host = miru.TextInput(label="host", placeholder="localhost", value=settings["host"])
+    port = miru.TextInput(label="port", placeholder="11434", value=settings["port"])
+    model = miru.TextInput(
+        label="model",
+        placeholder="codellama:13b-instruct-q4_1",
+        value=settings["model"],
+    )
+    temperature = miru.TextInput(
+        label="temperature", placeholder="0.6", value=settings["temperature"]
+    )
+
+    async def callback(self, ctx: miru.ModalContext) -> None:
+        if (
+            self.host.value
+            and self.port.value
+            and self.model.value
+            and self.temperature.value
+        ):
+            try:
+                data_manager.add_element_to_json(
+                    "data.json", ["ai", "host"], self.host.value
+                )
+                data_manager.add_element_to_json(
+                    "data.json", ["ai", "port"], self.port.value
+                )
+                data_manager.add_element_to_json(
+                    "data.json", ["ai", "model"], self.model.value
+                )
+                data_manager.add_element_to_json(
+                    "data.json", ["ai", "temperature"], self.temperature.value
+                )
+                await ctx.respond("Settings saved!")
+                return
+            except:
+                await ctx.respond("Something went wrong")
+                return
+        await ctx.respond("All fields must be filled")
+
+
+@plugin.command
+@lightbulb.command("ai", "Bootleg Ollama module")
+@lightbulb.implements(lightbulb.SlashCommandGroup)
+async def ai(ctx: lightbulb.Context) -> None:
+    pass
+
+
+@ai.child
+@lightbulb.command("settings", "ollama settings menu")
+@lightbulb.implements(lightbulb.SlashSubCommand)
+@administration_only
+async def settings(ctx: lightbulb.SlashContext) -> None:
+    modal = SettingsModal("Ollama Settings")
+    await modal.send(ctx.interaction)
+    await modal.wait()
+
+
+@ai.child
+@lightbulb.option("prompt", "prompt", str, required=True)
+@lightbulb.command("prompt", "Prompt")
+@lightbulb.implements(lightbulb.SlashSubCommand)
+@administration_only
+async def ai_prompt(ctx: lightbulb.Context) -> None:
+    response_content = ""
+    x = 0
+    template = """Answer the question at the end by following these rules:
+1. Keep your responses under 2000 characters.
+2. If you don't know the answer, just say that you don't know, try to not make up an answer.
+3. surround everything outside code blocks with double apostrophes (``)
+Question:
+"""
+    async with ctx.bot.rest.trigger_typing(ctx.channel_id):
+        message = await ctx.respond(f"> {ctx.options.prompt}")
+        for buffer, part in buffered_call(template + ctx.options.prompt):
+            x += 1
+            if x > 5:
+                await ctx.bot.rest.trigger_typing(ctx.channel_id)
+                x = 0
+            response_content += buffer
+            await message.edit(f"> {ctx.options.prompt} \n\n{response_content}")
 
 
 def call(prompt) -> dict:
@@ -41,73 +125,6 @@ def buffered_call(prompt) -> dict:
         if len(buffer) >= 32:
             yield buffer, part
             buffer = ""
-
-
-@plugin.command
-@lightbulb.command("ai", "Bootleg Ollama module")
-@lightbulb.implements(lightbulb.SlashCommandGroup)
-async def ai(ctx: lightbulb.Context) -> None:
-    pass
-
-
-@ai.child
-@lightbulb.command("set", "settings")
-@lightbulb.implements(lightbulb.SlashSubGroup)
-async def set(ctx: lightbulb.Context) -> None:
-    pass
-
-
-@set.child
-@lightbulb.option("address", "localhost by default", str, required=True)
-@lightbulb.command("host", "adress of the ollama api")
-@lightbulb.implements(lightbulb.SlashSubCommand)
-@administration_only
-async def ai_host(ctx: lightbulb.Context) -> None:
-    try:
-        data_manager.add_element_to_json(
-            "data.json", ["ai", "host"], ctx.options.address
-        )
-        await ctx.respond(f"address ``{ctx.options.address}`` saved")
-    except:
-        await error(ctx, "ai set host", "Something went wrong")
-
-
-@set.child
-@lightbulb.option("port", "11434 by default", int, required=True)
-@lightbulb.command("port", "port of the ollama api")
-@lightbulb.implements(lightbulb.SlashSubCommand)
-@administration_only
-async def ai_port(ctx: lightbulb.Context) -> None:
-    try:
-        data_manager.add_element_to_json("data.json", ["ai", "port"], ctx.options.port)
-        await ctx.respond(f"port ``{ctx.options.port}`` saved")
-    except:
-        await error(ctx, "ai set port", "Something went wrong")
-
-
-@ai.child
-@lightbulb.option("prompt", "prompt", str, required=True)
-@lightbulb.command("prompt", "Prompt")
-@lightbulb.implements(lightbulb.SlashSubCommand)
-@administration_only
-async def ai_prompt(ctx: lightbulb.Context) -> None:
-    response_content = ""
-    message = await ctx.respond(f"> {ctx.options.prompt}")
-    await ctx.bot.rest.trigger_typing(ctx.channel_id)
-    x = 0
-    template = """Answer the question at the end by following these rules:
-1. Keep your responses under 2000 characters.
-2. If you don't know the answer, just say that you don't know, try to not make up an answer.
-3. surround everything outside code blocks with double apostrophes (``)
-Question:
-"""
-    for buffer, part in buffered_call(template + ctx.options.prompt):
-        x += 1
-        if x > 5:
-            await ctx.bot.rest.trigger_typing(ctx.channel_id)
-            x = 0
-        response_content += buffer
-        await message.edit(f"> {ctx.options.prompt} \n\n{response_content}")
 
 
 def load(bot):
