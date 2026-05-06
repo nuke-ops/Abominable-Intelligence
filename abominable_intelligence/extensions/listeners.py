e1f0e2fde71eedc73686da60057e927bb311d740
Maksxpl <maks.kozon@gmail.com>
2026-05-06 18:27:50 +0200

    Update listeners.py

--- abominable_intelligence/extensions/listeners.py
+++ abominable_intelligence/extensions/listeners.py
@@ -1,30 +1,7 @@
-import logging
-import os
-import sys
-import traceback
-
-import hikari
 import lightbulb
 from extensions.core import error
 
 loader = lightbulb.Loader()
-
-
-@loader.listener(hikari.StartingEvent)
-async def on_ready(event: hikari.StartingEvent) -> None:
-    try:
-        print("Bot started, I think")
-        logging.info("Abominable intelligence has started!")
-
-        channel_id = os.environ.get("BOT_RESTARTED_CHANNEL")
-        if channel_id:
-            del os.environ["BOT_RESTARTED_CHANNEL"]
-            await event.app.rest.create_message(
-                channel=int(channel_id), content="Restart succeeded"
-            )
-
-    except Exception:
-        traceback.print_exc()
 
 
 @loader.error_handler
